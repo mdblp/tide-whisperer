ee315da2dad55c824b4cc8790c0eeda4fac48b46
Fabrice Sougey - Diabeloop <fabrice@diabeloop.fr>
2020-10-28 13:03:08 +0100

    Review OS Signals handling

--- tide-whisperer.go
+++ tide-whisperer.go
@@ -152,18 +152,15 @@ func main() {
 	}
 	hakkenClient.Publish(&config.Service)
 
-	signals := make(chan os.Signal, 40)
-	signal.Notify(signals)
+	// Wait for SIGINT (Ctrl+C) or SIGTERM to stop the service
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		for {
-			sig := <-signals
-			logger.Printf("Got signal [%s]", sig)
-
-			if sig == syscall.SIGINT || sig == syscall.SIGTERM {
-				storage.Close()
-				server.Close()
-				done <- true
-			}
+			<-sigc
+			storage.Close()
+			server.Close()
+			done <- true
 		}
 	}()
 
