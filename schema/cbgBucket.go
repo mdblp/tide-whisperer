039c9b3d4ee2daa5e5141285378eff2f91c1f6a6
Nasser <nasser.srairi@diabeloop.fr>
2021-10-04 11:51:52 +0200

    add new schema

---
+++ schema/cbgBucket.go
@@ -0,0 +1,22 @@
+package schema
+
+import "time"
+
+type (
+	CbgBucket struct {
+		Id               string    `bson:"_id,omitempty"`
+		CreatedTimeStamp time.Time `bson:"createdTimestamp,omitempty"`
+		UserId           string    `bson:"userId,omitempty"`
+		Day              time.Time `bson:"day,omitempty"` // ie: 2021-09-28
+		Measurements     []Sample  `bson:"measurements"`
+	}
+
+	Sample struct {
+		Value            float64   `bson:"value,omitempty"`
+		Units            string    `bson:"units,omitempty"`
+		CreatedTimeStamp time.Time `bson:"createdTimestamp,omitempty"`
+		TimeStamp        time.Time `bson:"timestamp,omitempty"`
+		Timezone         string    `bson:"timezone,omitempty"`
+		TimezoneOffset   int       `bson:"timezoneOffset,omitempty"`
+	}
+)
