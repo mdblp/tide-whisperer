a0b760c14ede2913e3ca3d1f0eca1c1a19f11541
Nasser <nasser.srairi@diabeloop.fr>
2021-10-04 14:00:00 +0200

    fix PR review

--- schema/cbgBucket.go
+++ schema/cbgBucket.go
@@ -5,7 +5,7 @@ import "time"
 type (
 	CbgBucket struct {
 		Id               string    `bson:"_id,omitempty"`
-		CreatedTimeStamp time.Time `bson:"createdTimestamp,omitempty"`
+		CreatedTimestamp time.Time `bson:"createdTimestamp,omitempty"`
 		UserId           string    `bson:"userId,omitempty"`
 		Day              time.Time `bson:"day,omitempty"` // ie: 2021-09-28
 		Measurements     []Sample  `bson:"measurements"`
@@ -14,7 +14,7 @@ type (
 	Sample struct {
 		Value            float64   `bson:"value,omitempty"`
 		Units            string    `bson:"units,omitempty"`
-		CreatedTimeStamp time.Time `bson:"createdTimestamp,omitempty"`
+		CreatedTimestamp time.Time `bson:"createdTimestamp,omitempty"`
 		TimeStamp        time.Time `bson:"timestamp,omitempty"`
 		Timezone         string    `bson:"timezone,omitempty"`
 		TimezoneOffset   int       `bson:"timezoneOffset,omitempty"`
