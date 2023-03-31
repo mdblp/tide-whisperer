fc681af348aeddbc9636e281f345a507448a09f2
Yann Blanc <yann.blanc@diabeloop.fr>
2023-03-31 09:49:29 +0200

    var name same as other controller

--- qa/test.sh
+++ qa/test.sh
@@ -7,10 +7,10 @@ fi
 
 go test -v -race -cover -coverprofile=coverage.out ./... 2>&1 > test-result.txt
 RET=$?
-cat test-result.txt
-cat test-result.txt | go-junit-report > test-report.xml
-cat test-report.xml
-rm -f /tmp/coverage.html
-go tool cover -html=coverage.out -o coverage.html
+#cat test-result.txt
+#cat test-result.txt | go-junit-report > test-report.xml
+#cat test-report.xml
+#rm -f /tmp/coverage.html
+#go tool cover -html=coverage.out -o coverage.html
 
 exit $RET
