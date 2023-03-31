9eb2a8d019f86c2fb0b868d03825e6195eaf598d
Yann Blanc <yann.blanc@diabeloop.fr>
2023-03-31 14:54:00 +0200

    remove race in test

--- qa/test.sh
+++ qa/test.sh
@@ -5,12 +5,12 @@ if [ "$?" != "0" ]; then
   go install github.com/jstemmer/go-junit-report@latest
 fi
 
-go test -v -race -cover -coverprofile=coverage.out ./... 2>&1 > test-result.txt
+go test -v  -cover -coverprofile=coverage.out ./... 2>&1 > test-result.txt
 RET=$?
-#cat test-result.txt
-#cat test-result.txt | go-junit-report > test-report.xml
-#cat test-report.xml
-#rm -f /tmp/coverage.html
-#go tool cover -html=coverage.out -o coverage.html
+cat test-result.txt
+cat test-result.txt | go-junit-report > test-report.xml
+cat test-report.xml
+rm -f /tmp/coverage.html
+go tool cover -html=coverage.out -o coverage.html
 
 exit $RET
